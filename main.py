bbe35a0d2534a2a25cc273c27ddaa62a6022b683
Evgeny <jetfree@yandex.ru>
2022-03-23 16:51:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def update_datetime(new_ads):
     global last_updated_datetime
     sorted_list = sorted(new_ads, key=lambda x: x.get_time(), reverse=True)
     last_updated_datetime = sorted_list[0].get_time()
-    print(last_updated_datetime)
+    print(f"New ad was found. Time: {last_updated_datetime}")
 
 
 def construct_msg(ad):
